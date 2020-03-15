f39d41539ca29ab1979a5be6aa201c49e7869a2a
iordmanu <61532516+iordmanu@users.noreply.github.com>
2020-03-15 11:44:32 -0400

    Add files via upload

---
+++ CALCULATOR_TERM.py
@@ -0,0 +1,35 @@
+def add(a,b):
+    return a+b
+def remove(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def division(a,b):
+    if (b == 0):
+        print("Operatie inutila")
+    else:
+        return a/b
+def print_partial(value):
+    print("Rezultat partial: ", value)
+
+operatie = 0
+total = int(input())
+while operatie != "=":
+    operatie = input()
+    if operatie != "=":
+        alt_numar = int(input())
+    if operatie == "+":
+        total = add(total, alt_numar)
+        print_partial(total)
+    elif operatie == "-":
+        total = remove(total, alt_numar)
+        print_partial(total)
+    elif operatie == "*":
+        total = multiply(total, alt_numar)
+        print_partial(total)
+    elif operatie == "/":
+        total = division(total, alt_numar)
+        print_partial(total)
+else:
+    print("Rezultat final: ", total)
+    print(["programul s-a incheiat"])
