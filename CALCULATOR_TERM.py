0979d1ae84b1fa709bfca42463198597c27aec32
iordmanu <61532516+iordmanu@users.noreply.github.com>
2020-03-26 09:34:13 -0400

    Update CALCULATOR_TERM.py

--- CALCULATOR_TERM.py
+++ CALCULATOR_TERM.py
@@ -1,26 +1,30 @@
-def add(a,b):
+  def add(a,b):
     return a+b
 def remove(a,b):
     return a-b
 def multiply(a,b):
     return a*b
 def division(a,b):
-    if (b == 0):
+    if b == 0:
         print("Operatie inutila")
+        return a
     else:
         return a/b
+
 def print_partial(value):
     print("Rezultat partial: ", value)
 
 operatie = 0
-total = int(input())
+total = float(input())
 while operatie != "=":
     operatie = input()
     if operatie != "=":
-        alt_numar = int(input())
+        alt_numar = float(input())
+
     if operatie == "+":
         total = add(total, alt_numar)
         print_partial(total)
+
     elif operatie == "-":
         total = remove(total, alt_numar)
         print_partial(total)
@@ -30,6 +34,9 @@ while operatie != "=":
     elif operatie == "/":
         total = division(total, alt_numar)
         print_partial(total)
-else:
-    print("Rezultat final: ", total)
-    print(["programul s-a incheiat"])
+
+    else:
+        print("Rezultat final: ", total)
+        print(["programul s-a incheiat"])
+
+
